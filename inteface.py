62eb6f4c8084e931ace2723e281dd68cdff49c68
Felipe Mendonça <97059009+felipeadsm@users.noreply.github.com>
2023-11-16 17:21:07 -0300

    update

--- inteface.py
+++ inteface.py
@@ -35,6 +35,14 @@ class Interface:
         self.ted_default_gateway.pack()
         frame_default_gateway.pack()
 
+        # Label 'DNS Server' and TextEdit 'DNS Server content'
+        frame_dns_server = Frame(main_frame)
+        self.lb_dns_server = Label(frame_dns_server, text='DNS Server:', font=('Helvetica', 16))
+        self.ted_dns_server = Entry(frame_dns_server, width=50, font=('Helvetica', 12), justify='center')
+        self.lb_dns_server.pack()
+        self.ted_dns_server.pack()
+        frame_dns_server.pack()
+
         # Radio Buttons 'Static' and 'DHCP'
         frame_rds = Frame(main_frame)
         self.rd_variable = StringVar()
@@ -57,29 +65,42 @@ class Interface:
             self.ted_ip_addr.config(state='disabled')
             self.ted_subnet_mask.config(state='disabled')
             self.ted_default_gateway.config(state='disabled')
+            self.ted_dns_server.config(state='disabled')
 
         window.mainloop()
 
     def change_ted_state(self):
         if self.rd_variable.get() == 'DHCP':
+
             self.ted_ip_addr.delete(0, END)
             self.ted_ip_addr.config(state='disabled')
+
             self.ted_subnet_mask.delete(0, END)
             self.ted_subnet_mask.config(state='disabled')
+
             self.ted_default_gateway.delete(0, END)
             self.ted_default_gateway.config(state='disabled')
+
+            self.ted_dns_server.delete(0, END)
+            self.ted_dns_server.config(state='disabled')
         else:
             self.ted_ip_addr.config(state='enabled')
-            self.ted_ip_addr.insert(0, '192.169.160.1')
+            self.ted_ip_addr.insert(0, '192.169.160.225')
+
             self.ted_subnet_mask.config(state='enabled')
             self.ted_subnet_mask.insert(0, '255.255.255.224')
+
             self.ted_default_gateway.config(state='enabled')
             self.ted_default_gateway.insert(0, '192.168.160.1')
+
+            self.ted_dns_server.config(state='enabled')
+            self.ted_dns_server.insert(0, '192.168.155.252')
 
     def get_static_variables(self):
         ip_addr = self.ted_ip_addr.get()
         subnet_mask = self.ted_subnet_mask.get()
         default_gateway = self.ted_default_gateway.get()
+        dns_server = self.ted_dns_server.get()
 
         with open("static_ip.bat", "r") as bat_file:
             data = bat_file.readlines()
@@ -87,6 +108,7 @@ class Interface:
             data[0] = f'set ip_addr={ip_addr}\n'
             data[1] = f'set subnet_mask={subnet_mask}\n'
             data[2] = f'set default_gateway={default_gateway}\n'
+            data[3] = f'set dns_server={dns_server}\n'
 
         with open("static_ip.bat", "w") as bat_file:
             bat_file.writelines(data)
